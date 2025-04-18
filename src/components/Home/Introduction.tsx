bc267864fccde28848a56df536c729521f501204
LongMyNgoc <longmyngoc2004@gmail.com>
2025-04-18 17:50:10 +0700

    Fixed Error Vercel Introduction

--- src/components/Home/Introduction.tsx
+++ src/components/Home/Introduction.tsx
@@ -6,12 +6,12 @@ const Introduction = () => {
   return (
     <div className="text-white max-w-3xl mt-6 px-4 md:px-0">
       <h1 className="text-3xl md:text-5xl font-bold mb-4">
-        👋 Hi, I'm <span className="text-yellow-400">Nguyễn Phi Long</span>
+        👋 Hi, I&apos;m <span className="text-yellow-400">Nguyễn Phi Long</span>
       </h1>
       <p className="text-lg md:text-xl leading-relaxed">
-        I'm a <span className="text-yellow-300 font-semibold">Software Developer</span> passionate about building clean, scalable, and user-focused applications across <span className="text-yellow-300">web</span>, <span className="text-yellow-300">mobile</span>, and <span className="text-yellow-300">game</span> platforms. I enjoy working with technologies like <span className="text-yellow-300">React</span>, <span className="text-yellow-300">Next.js</span>, <span className="text-yellow-300">React Native</span>, <span className="text-yellow-300">FastAPI</span>, <span className="text-yellow-300">Express.js</span>, <span className="text-yellow-300">NestJS</span>, and <span className="text-yellow-300">Unity</span>.  
+        I&apos;m a <span className="text-yellow-300 font-semibold">Software Developer</span> passionate about building clean, scalable, and user-focused applications across <span className="text-yellow-300">web</span>, <span className="text-yellow-300">mobile</span>, and <span className="text-yellow-300">game</span> platforms. I enjoy working with technologies like <span className="text-yellow-300">React</span>, <span className="text-yellow-300">Next.js</span>, <span className="text-yellow-300">React Native</span>, <span className="text-yellow-300">FastAPI</span>, <span className="text-yellow-300">Express.js</span>, <span className="text-yellow-300">NestJS</span>, and <span className="text-yellow-300">Unity</span>.
         <br /><br />
-        I'm always excited to turn ideas into reality and continuously explore new tools in the ever-evolving tech world.
+        I&apos;m always excited to turn ideas into reality and continuously explore new tools in the ever-evolving tech world.
       </p>
     </div>
   );
