044bbb4499687f571ec7cda35dab07ed5cb2bc48
LongMyNgoc <longmyngoc2004@gmail.com>
2025-04-24 23:04:50 +0700

    Fixed Error Vercel useSearchParams

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -1,12 +1,11 @@
 "use client"
+import { Suspense } from "react"; // Nhập Suspense từ React
 import MainNavbar from "@/components/Layout/Navbar/MainNabar";
 import Footer from "@/components/Layout/Footer/Footer";
 import { useSearchParams } from 'next/navigation';
 import Feedback from "@/components/Projects/Feedback";
 
 export default function FeedbackPage() {
-  const searchParams = useSearchParams();
-  const name = searchParams.get('name')!;
   return (
     <div
       className="min-h-screen flex flex-col bg-cover bg-center bg-no-repeat"
@@ -15,10 +14,21 @@ export default function FeedbackPage() {
       <MainNavbar />
 
       <div className="pt-10 md:pl-6 flex flex-col md:flex-row items-start gap-10 flex-grow mb-16">
-      <Feedback name={name} />
+        {/* Bọc Feedback trong Suspense */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <FeedbackWithSearchParams />
+        </Suspense>
       </div>
 
       <Footer />
     </div>
   );
+}
+
+// Component phụ để sử dụng useSearchParams
+function FeedbackWithSearchParams() {
+  const searchParams = useSearchParams();
+  const name = searchParams.get('name')!;  // Lấy tham số 'name' từ URL
+  
+  return <Feedback name={name} />;
 }
